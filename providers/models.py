273f58b50c7073bbc12827eb06837d0cce967d69
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-03-21 15:56:43 -0300

    feat: add the id field to the Provider model

--- providers/models.py
+++ providers/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 
 class Provider(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     instagram = models.CharField(max_length=255)
     facebook = models.CharField(max_length=255)
     description = models.TextField()
