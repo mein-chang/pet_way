47d1cc8835192b61ba138bea8b09aeed16a72e6d
Jorge Willian <jorgewillianpaez@gmail.com>
2022-03-21 17:01:40 -0300

    feat: adding apps and user model to the project settings

--- petway/settings.py
+++ petway/settings.py
@@ -44,8 +44,12 @@ THIRD_PARTY_APPS = [
 ]
 
 MY_APPS = [
-    'services',
+    'users',
+    'pets',
+    'addresses',
     'providers',
+    'services',
+    'ratings',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
@@ -132,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
