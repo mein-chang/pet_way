cedd8b22413baa9815f631e69f343d49d688566c
Mein Mieko Chang <mein_chang@hotmail.com>
2022-03-22 14:01:17 -0300

    feat: address create and list

--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from .models import Address
+from .serializers import AddressSerializer
+
+
+class AddressListCreateView(ListCreateAPIView):
+
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+    authentication_classes = [TokenAuthentication]
+    # permission_classes = []
+
+
+class AddressRetrieveUpdateView(RetrieveUpdateAPIView):
+
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+    lookup_url_kwarg = "address_id"
+
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = []
