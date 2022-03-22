55e91668926a0e0e08999b1953332d4c5b0e2673
joaopedro <jpss7693@gmai.com>
2022-03-22 13:05:26 -0300

    feat: creating the view and serializer part of the pet table

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from .models import Pet
+# from .serializers import PetSerializer
+# from rest_framework.authentication import TokenAuthentication
+# from rest_framework.permissions import IsAuthenticated
+# from permissions import IsClientOrReadOnly, IsOwnerPet
+# # Create your views here.
 
-# Create your views here.
+# class PetCreateListView(ListCreateAPIView):
+    
+#     queryset = Pet.objects.all()
+#     serializer_class = PetSerializer
+
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated,IsClientOrReadOnly]
+    
+    
+# class PetRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    
+#     queryset = Pet.objects.all()
+#     serializer_class = PetSerializer
+
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsClientOrReadOnly, IsOwnerPet]
+    
+    
+#     lookup_url_kwarg = 'pet_id'
