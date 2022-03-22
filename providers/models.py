6e903a9686454da8534b351b8e053cb742aa44d8
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-03-22 09:48:57 -0300

    feat: create ProviderSerializer

--- providers/models.py
+++ providers/models.py
@@ -7,5 +7,4 @@ class Provider(models.Model):
     instagram = models.CharField(max_length=255)
     facebook = models.CharField(max_length=255)
     description = models.TextField()
-
-    # user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
