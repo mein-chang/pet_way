7ff51fa9275ae9152bf933505dbd87c83cd32aa6
Miqueias <miqs_carvalho@hotmail.com>
2022-03-23 18:08:11 -0300

    feat: OrderTable initial settings

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Order
+from rest_framework import generics
+from .serializers import OrderSerializer
 
-# Create your views here.
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderRetrieveView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    lookup_url_kwarg = "order_id"
