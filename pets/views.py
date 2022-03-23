89822bd5852bef87b36b497ce6a9625a4c45706e
joaopedro <jpss7693@gmai.com>
2022-03-23 14:26:40 -0300

    feat: creating pet table exceptions

--- pets/views.py
+++ pets/views.py
@@ -1,28 +1,39 @@
-# from django.shortcuts import render
-# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-# from .models import Pet
-# from .serializers import PetSerializer
-# from rest_framework.authentication import TokenAuthentication
-# from rest_framework.permissions import IsAuthenticated
-# from permissions import IsClientOrReadOnly, IsOwnerPet
-# # Create your views here.
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Pet
+from .serializers import PetSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsClientOrReadOnly, IsOwnerPet
+# Create your views here.
 
-# class PetCreateListView(ListCreateAPIView):
+class PetCreateListView(ListCreateAPIView):
     
-#     queryset = Pet.objects.all()
-#     serializer_class = PetSerializer
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
 
-#     authentication_classes = [TokenAuthentication]
-#     permission_classes = [IsAuthenticated,IsClientOrReadOnly]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated,IsClientOrReadOnly]
     
     
-# class PetRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    
-#     queryset = Pet.objects.all()
-#     serializer_class = PetSerializer
+    def get_queryset(self):
+        request_user = self.request.user.id
+        
+        if request_user is not None:
+            queryset = Pet.objects.filter(user=request_user)
 
-#     authentication_classes = [TokenAuthentication]
-#     permission_classes = [IsClientOrReadOnly, IsOwnerPet]
+            return queryset
+        
+        return super().get_queryset()
     
     
-#     lookup_url_kwarg = 'pet_id'
+class PetRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsClientOrReadOnly, IsOwnerPet]
+    
+    
+    lookup_url_kwarg = 'pet_id'
