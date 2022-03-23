17a3ff92d5b5c7c454e1da7f99b5aeab3967c52b
Jorge Willian <jorgewillianpaez@gmail.com>
2022-03-23 09:00:00 -0300

    fix: fixing user view at urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.urls import path
-from users.views import ListCreateAPIView, UserLoginView
+from users.views import UserView, UserLoginView
 
 
 urlpatterns = [
-    path('users/', ListCreateAPIView.as_view()),
+    path('users/', UserView.as_view()),
     path('login/', UserLoginView.as_view())
 ]
