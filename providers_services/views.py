cbac72a84ea05ba159d195b17d7b97bf01c4604b
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-03-23 18:59:41 -0300

    feat: add list providers services by service type

--- providers_services/views.py
+++ providers_services/views.py
@@ -1,10 +1,11 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from .models import ProviderService
-from .serializers import ProviderServiceSerializer
+from .serializers import BasicProviderServiceSerializer, ProviderServiceSerializer
 from rest_framework.authentication import TokenAuthentication
 from providers_info.permissions import IsProvider
 from users.models import User
 from django.shortcuts import get_object_or_404
+from .exceptions import IdIsNotProvider
 
 
 class ProviderServiceListCreateView(ListCreateAPIView):
@@ -19,6 +20,14 @@ class ProviderServiceListCreateView(ListCreateAPIView):
         return serializer
 
 
+    def filter_queryset(self, queryset):
+        if 'type' in self.request.query_params:
+            service_type = self.request.GET.get('type').replace(self.request.GET.get('type')[3:4], ' ')
+            queryset = queryset.filter(type__icontains=service_type)
+
+        return super().filter_queryset(queryset)
+
+
 class ProviderServiceRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = ProviderService.objects.all()
     serializer_class = ProviderServiceSerializer
@@ -30,13 +39,17 @@ class ProviderServiceRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
 
 class ProviderServiceListByProvider(ListAPIView):
     queryset = ProviderService.objects.all()
-    serializer_class = ProviderServiceSerializer
+    serializer_class = BasicProviderServiceSerializer
 
     lookup_url_kwarg = 'provider_id'
 
 
     def get_queryset(self):
-        provider = get_object_or_404(User, id=self.kwargs['provider_id'])
-        queryset = ProviderService.objects.filter(provider=provider)
+        user = get_object_or_404(User, id=self.kwargs['provider_id'])
+        
+        if not user.is_provider:
+            raise IdIsNotProvider() 
+
+        queryset = ProviderService.objects.filter(provider=user)
         return queryset
         
