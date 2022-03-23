910080988cc4863c601679dfaeb861b0ba98c5d9
Miqueias <miqs_carvalho@hotmail.com>
2022-03-23 18:13:45 -0300

    feat: initial rating routes settings

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RaitingListCreateView
-
+from .views import RatingPetView,RatingProviderView
 
 urlpatterns = [
-    path('ratings/', RaitingListCreateView.as_view())
+    path('orders/<str:order_id>/rating/pet/',RatingPetView.as_view()),
+    path('orders/<str:order_id>/rating/provider/',RatingProviderView.as_view()),
+    
 ]
