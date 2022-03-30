7109bfabddc2220e24f879dfdb644aab3589559b
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-03-30 10:08:12 -0300

    fix: add verification if user is authenticated

--- pets/permissions.py
+++ pets/permissions.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsClientOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         if request.user.is_provider == False and request.user.is_admin == False:
             return True
 
